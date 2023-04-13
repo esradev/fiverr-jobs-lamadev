eb8c4a038fa0aa52efcf5c35a4e6abe681a84a6b
Wpstorm <wpstormdev@gmail.com>
2023-04-13 06:52:34 +0330

    feat: Completed navbar UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,52 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
 import Navbar from "./components/navbar/Navbar.jsx";
+import Home from "./pages/home/Home.jsx";
+import Footer from "./components/footer/Footer.jsx";
+import Gigs from "./pages/gigs/Gigs.jsx";
+import Gig from "./pages/gig/Gig.jsx";
+import Orders from "./pages/orders/Orders.jsx";
+import MyGigs from "./pages/myGigs/MyGigs.jsx";
+import Add from "./pages/add/Add.jsx";
+import Message from "./pages/message/Message.jsx";
+import Messages from "./pages/messages/Messages.jsx";
+
+import "./app.scss";
 
 function App() {
+  const Layout = () => {
+    return (
+      <div className="app">
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "/gigs", element: <Gigs /> },
+        { path: "/gig/:id", element: <Gig /> },
+        { path: "/orders", element: <Orders /> },
+        { path: "/my-gigs", element: <MyGigs /> },
+        { path: "/add-new-gig", element: <Add /> },
+        { path: "/messages", element: <Messages /> },
+        { path: "/message/:id", element: <Message /> },
+      ],
+    },
+  ]);
 
   return (
     <div>
-      <Navbar />
+      <RouterProvider router={router} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,25 +1,79 @@
-import React from 'react';
-import "./Navbar.scss"
+import React, { useEffect, useState } from "react";
+import "./Navbar.scss";
+import {Link, useLocation} from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <div className='navbar'>
-            <div className="container">
-                <div className="logo">
-                    <span className='text'>Fiverr</span>
-                    <span className='dot'>.</span>
-                </div>
-                <div className="links">
-                    <span>Fiverr Business</span>
-                    <span>Explore</span>
-                    <span>English</span>
-                    <span>Sing in</span>
-                    <span>Become a Seller</span>
-                    <button>Join</button>
-                </div>
-            </div>
+  const [active, setActive] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const {pathname} = useLocation()
+
+  const isActive = () => {
+    window.scrollY > 0 ? setActive(true) : setActive(false);
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", isActive);
+    return () => {
+      window.removeEventListener("scroll", isActive);
+    };
+  }, []);
+
+  const currentUser = {
+    id: 1,
+    username: "wpstorm",
+    isSeller: true,
+  };
+
+  return (
+    <div className={active || pathname !== "/" ? "navbar active" : "navbar"}>
+      <div className="container">
+        <div className="logo">
+          <Link to="/" className="link">
+          <span className="text">Fiverr</span>
+          </Link>
+          <span className="dot">.</span>
         </div>
-    );
+        <div className="links">
+          <span>Fiverr Business</span>
+          <span>Explore</span>
+          <span>English</span>
+          <span>Sing in</span>
+          {!currentUser?.isSeller && <span>Become a Seller</span>}
+          {!currentUser && <button>Join</button>}
+          {currentUser && (
+            <div className="user" onClick={()=>setOpen(!open)}>
+              <img src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1000&q=60" alt="" />
+              <span>{currentUser?.username}</span>
+              {open &&
+                  <div className="options">
+                {currentUser?.isSeller && (
+                    <>
+                      <Link className="link" to="/gigs">Gigs</Link >
+                      <Link className="link" to="/add-new-gig">Add New Gig</Link >
+                    </>
+                )}
+                <Link className="link" to="/orders">Orders</Link >
+                <Link className="link" to="/messages">Messages</Link >
+                <Link className="link" to="/">Logout</Link >
+              </div>}
+            </div>
+          )}
+        </div>
+      </div>
+      {active || pathname !=="/" && (
+        <>
+          <hr />
+          <div className="menu">
+            <Link className="link" to="">Fiverr Business</Link >
+            <Link className="link" to="">Explore</Link >
+            <Link className="link" to="">English</Link >
+            <Link className="link" to="">Sing in</Link >
+            <Link className="link" to="">Become a Seller</Link >
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Navbar;
